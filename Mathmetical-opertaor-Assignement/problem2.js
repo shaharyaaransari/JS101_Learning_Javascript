2e98844f922a007e60f8be1332e15eb890515790
shaharyaar <shaharyaaransari786@gmail.com>
2022-09-13 17:22:19 +0530

    Mathametical problem

---
+++ Mathmetical-opertaor-Assignement/problem2.js
@@ -0,0 +1,12 @@
+let one = 1;
+let two =2;
+let three = 3;
+let four = 4;
+let five = 5;
+one= 1*2;
+two = 2*2;
+three = 3*2;
+four= 4*2;
+five = 5*2;
+let sum = one+two+three+four+five;
+console.log (sum);
---
+++ Mathmetical-opertaor-Assignement/problem3.js
@@ -0,0 +1,13 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+ let five = 5;
+let six= 6;
+two = 2*2;
+three = 3*3;
+four = 4*3;
+five= 5*2;
+six = 6*2;
+let sum = one+two+three+four+five+six;
+console.log(sum);
